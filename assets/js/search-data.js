2febf72d9efc38ffcd8d4b07b33911dafa300c6c
Anupam1223 <50481498+Anupam1223@users.noreply.github.com>
2026-07-20 18:20:18 +0000

    Deploying to gh-pages from @ Anupam1223/anupam1223.github.io@07bd6f61710f475f905884589b1a17c011e2c1ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "Ongoing projects and expertise — click any project to explore it interactively.",
+          description: "Ongoing projects and expertise — Select a project to explore its features. Be sure to click through the various tabs, and don&#39;t forget to scroll to the bottom to find the button for the next page!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
